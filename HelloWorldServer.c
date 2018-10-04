18522cf3ab3a0342225ebaaedf67bded652491c3
211 <211@localhost.localdomain>
2018-10-04 11:50:17 +0900

    [10.04] 5-2까지 완료

--- HelloWorldServer.c
+++ HelloWorldServer.c
@@ -9,7 +9,6 @@
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
 char buffer[100] = "Hi, I'm server\n";
- 
 main( )
 {
 	int   c_socket, s_socket;
@@ -17,7 +16,7 @@ main( )
 	int   len;
 	int   n;
 	int rcvLen;
-	char rcvBuffer[100];
+	char rcvBuffer[100], rcv[100], *tok;
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
@@ -44,11 +43,41 @@ main( )
 		while(1){
 			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
 			rcvBuffer[rcvLen] = '\0';
+			rcv[rcvLen] = '\0';
 			printf("[%s] received\n", rcvBuffer);
 			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
 				break;
+			
+
+			if(strcmp(rcvBuffer, "안녕하세요") == 0){
+				strcpy(rcv, "안녕하세요 만나서 반가워요.");
+				n = strlen(rcv);
+				write(c_socket, rcv, n);
+			}
+			else if(strcmp(rcvBuffer, "이름이뭐야?") == 0){
+				strcpy(rcv, "내이름은 전진수야.");
+				n = strlen(rcv);
+				write(c_socket, rcv, n);
+			}
+			else if(strcmp(rcvBuffer, "몇살이야?") == 0){
+				strcpy(rcv, "나는 29살이야.");
+				n = strlen(rcv);
+				write(c_socket, rcv, n);
+			}
+			else if(strncasecmp(rcvBuffer, "strlen ", 6) == 0){
+				strtok(rcvBuffer, " ");
+				printf("%s", tok);
+				tok = strtok(NULL, " ");
+			}
+			else if(strcmp(rcvBuffer, "이름이 뭐야?") == 0){
+				strcpy(rcv, "내이름은 전진수야.");
+				n = strlen(rcv);
+				write(c_socket, rcv, n);
+			}
+			else{
 			n = strlen(buffer);
 			write(c_socket, buffer, n);
+			}
 		}
 		close(c_socket);
 		if(!strncasecmp(rcvBuffer, "kill server", 11))
