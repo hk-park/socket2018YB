75d86bae27ac4acfa415184124cdfceb1dabe578
211 <211@localhost.localdomain>
2018-10-04 11:49:53 +0900

    [10.04]5-1

---
+++ ChatBotServer.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <string.h>
+#define PORT 9000
+
+char Hbuffer[100] = "안녕하세요. 만나서 반가워요\n";
+char Nbuffer[100] = "내 이름은 유요섭이야.\n";
+char Abuffer[100] = "나는 21살이야.\n";
+ 
+main( )
+{
+	int   c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int   len;
+	int   n, i;
+	int rcvLen;
+	char rcvBuffer[100];
+ 	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&s_addr, 0, sizeof(s_addr));
+	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+ 
+	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not Bind\n");
+		return -1;
+	}
+ 
+	if(listen(s_socket, 5) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+ 	
+	while(1) {
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		printf("Client is connected\n");
+		while(1){
+			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+			rcvBuffer[rcvLen] = '\0';
+			printf("[%s] received\n", rcvBuffer);
+			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
+				break;
+			if(strncasecmp(rcvBuffer, "안녕하세요.", 6)){
+				n = strlen(Hbuffer);
+				write(c_socket, Hbuffer, n);
+			}
+			else if(strncasecmp(rcvBuffer, "이름이 뭐야?", 7)){
+				n = strlen(Nbuffer);
+				write(c_socket, Nbuffer, n);
+			}
+			else if(strncasecmp(rcvBuffer, "몇 살이야?", 6)){
+				n = strlen(Abuffer);
+				write(c_socket, Abuffer, n);
+			}
+			else if(strncasecmp(rcvBuffer, "strcmp ", 7)){
+				n = strlen(rcvbuffer);				
+				for(i=7;i<n;i++){
+					if(rcvbuffer[i]
+			}
+		}
+		close(c_socket);
+		if(!strncasecmp(rcvBuffer, "kill server", 11))
+			break;
+	}	
+	close(s_socket);
+}
