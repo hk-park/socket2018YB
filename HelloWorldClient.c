e2983a2472bcf584f3eeb4096e748e79525a72fa
AndMeda <sydpdd101@naver.com>
2018-10-04 11:28:21 +0900

    나의 첫번째 소스코드

--- HelloWorldClient.c
+++ HelloWorldClient.c
@@ -3,7 +3,7 @@
 #include <sys/socket.h>
 #include <string.h>
 
-#define PORT 9000
+#define PORT 10000
 #define IPADDR "127.0.0.1"
 
 int main(){
@@ -11,36 +11,52 @@ int main(){
 	struct sockaddr_in c_addr;
 	int n;
 	char rcvBuffer[100];
-	char sendBuffer[100];
+	char buffer[100];
 	c_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&c_addr, 0, sizeof(c_addr));
 	c_addr.sin_addr.s_addr = inet_addr(IPADDR);
 	c_addr.sin_family = AF_INET;
 	c_addr.sin_port = htons(PORT);
-	
+
 	if(connect(c_socket, (struct sockaddr *) &c_addr, sizeof(c_addr)) == -1){
 		printf("[ERR] Cannot connect\n");
 		close(c_socket);
 		return -1;
 	}
-	//strcpy(sendBuffer, "Hi, I'm client\n");
+	//strcpy(buffer, "만일 내게 물어보면 나는 클라이언트\n");
 	while(1){
-		fgets(sendBuffer, sizeof(sendBuffer), stdin);
-		sendBuffer[strlen(sendBuffer)-1] = '\0';
-		write(c_socket, sendBuffer, strlen(sendBuffer));
-		if(strncasecmp(sendBuffer, "quit", 4) == 0 || strncasecmp(sendBuffer, "kill server", 11) == 0)
-			break;
-		n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-		if(n < 0){
-			printf("[ERR] Cannot read\n");
-			return -1;
-		}
-		rcvBuffer[n] = '\0';
-		printf("received Data: %s\n", rcvBuffer);
-		// 3-2. 서버로부터 받은 문자열 길이 출력
-		printf("received data length: %d\n", n);
-	}	
+	buffer[0] = '\0';
+	fgets(buffer, sizeof(buffer), stdin);
+	n = write(c_socket, buffer, strlen(buffer));
+	
+	if(!strcmp(buffer,"quit\n"))
+		break;
+	if(!strcmp(buffer,"kill server\n"))
+		break;
+
+
+	if(n < 0){
+		printf("[ERR] Cannot write\n");
+		return -1;
+	}
+
+	n = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+
+	if(n < 0){
+		printf("[ERR] Cannot read\n");
+		return -1;
+	}
+
+	rcvBuffer[n] = '\0';
+
+
+	printf("서버로부터 받은 문자열: %s", rcvBuffer);
+	printf("서버로부터 받은 문자열의 길이: %d\n", strlen(rcvBuffer));
+
+	}
+
+	
 	close(c_socket);
 	return 0;
 
--- HelloWorldServer.c
+++ HelloWorldServer.c
@@ -3,21 +3,17 @@
 #include <sys/socket.h>
 #include <string.h>
 // 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
-#define PORT 9000
-//#define PORT 10000
+//#define PORT 9000
+#define PORT 10000
  
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
-char buffer[100] = "Hi, I'm server\n";
- 
+char buffer[100];
 main( )
 {
 	int   c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
-	int   len;
-	int   n;
-	int rcvLen;
-	char rcvBuffer[100];
+	int   sock, len, str_len, n;
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
@@ -27,7 +23,7 @@ main( )
 	s_addr.sin_port = htons(PORT);
  
 	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
-		printf("Can not Bind\n");
+		
 		return -1;
 	}
  
@@ -35,24 +31,49 @@ main( )
 		printf("listen Fail\n");
 		return -1;
 	}
- 	
-	while(1) {
+
+
+while(1){
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
-		printf("Client is connected\n");
-		while(1){
-			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-			rcvBuffer[rcvLen] = '\0';
-			printf("[%s] received\n", rcvBuffer);
-			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
-				break;
-			n = strlen(buffer);
-			write(c_socket, buffer, n);
+
+	while(1) {
+		buffer[0]='\0';
+		n = read(c_socket, buffer, sizeof(buffer));
+		if(n < 0){
+			printf("[ERR] Cannot read\n");
+			return -1;
 		}
-		close(c_socket);
-		if(!strncasecmp(rcvBuffer, "kill server", 11))
+		buffer[n] = '\0';	
+		if(!strcmp(buffer,"quit\n"))
 			break;
+		if(!strcmp(buffer,"kill server\n")){
+			close(s_socket);
+			return -1 ;
+		}
+		printf("클라이언트로부터 받은 문자열: %s", buffer);
+		if(!strcmp(buffer,"안녕하세요.\n"))
+			strcpy(buffer, "안녕하세요 만나서 반가워요.\n");
+		else if(!strcmp(buffer,"이름이 머야?\n"))
+			strcpy(buffer, "내 이름은 Siri야.\n");
+		else if(!strcmp(buffer,"몇 살이야?\n"))
+			strcpy(buffer, "나는 XX살이야.\n");
+		else if(!strncasecmp(buffer, "strlen", 6)){
+			
+			sprintf(buffer, "%d", );
+			strcpy(buffer, strlen(buffer)-7);
+		}
+		else
+			strcpy(buffer, "만일 내게 물어보면 나는 서버\n");
+		write(c_socket, buffer, strlen(buffer));
 	}	
+
+	close(c_socket);
+}
 	close(s_socket);
 }
+
+
+
+
+
