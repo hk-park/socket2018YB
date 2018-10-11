db76e4d06910cd714599b81881dd86a98a27240d
dongyang <dongyang@localhost.localdomain>
2018-10-11 11:04:18 +0900

    파일 테스트 코드 추가

---
+++ file.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+        FILE *fp;
+        Char buffer[100];
+
+        fp = fopen("test.txt", "r");
+        if(fp) {
+                while(fgets(buffer, 100, (FILE *)fp))
+                        printf("%s", buffer);
+        }
+        fclose(fp);
+
+        int ret = system("mkdir testdir");
+        if(!ret)
+                printf("command Success!!\n");
+        else
+                printf("command Failed!\n");
+        return 0;
+}
+
