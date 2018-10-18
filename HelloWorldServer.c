32a4944b3a832772be2dc8e0da2a0df33b5dfa36
211 <211@localhost.localdomain>
2018-10-18 10:39:26 +0900

    [10.18]6-1,2 수정 및 모든 실습 완료

--- HelloWorldServer.c
+++ HelloWorldServer.c
@@ -5,19 +5,20 @@
 // 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
 #define PORT 9000
 //#define PORT 10000
+#define BUFSIZE 10000
  
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
-char buffer[100] = "Hi, I'm server\n";
+char buffer[BUFSIZE] = "Hi, I'm server\n";
  
 main( )
 {
 	int   c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
 	int   len;
-	int   n;
+	int   n, i;
 	int rcvLen;
-	char rcvBuffer[100];
+	char rcvBuffer[BUFSIZE];
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
@@ -72,10 +73,11 @@ main( )
 				else
 					sprintf(buffer, "%s와 %s는 다른 문자열입니다.", str[1], str[2]);
 			}
+			//6-1
 			else if(!strncasecmp(rcvBuffer, "readfile ", 9)){
 				char *Ftoken;
 				char *Fstr[2];
-				int i = 0;
+				i = 0;
 				FILE *fp; 
 					
 				Ftoken = strtok(rcvBuffer, " ");
@@ -84,32 +86,35 @@ main( )
 					Ftoken = strtok(NULL, " ");
 				}
 				
+				if(i<2) {
+					sprintf(buffer, "readfile 기능을 사용하기 위해서는 readfile<파일명> 형태로 입력하시오.");
+				}
 				fp = fopen(Fstr[1], "r");
+
 				if(fp){
 					while(fgets(buffer, 100, (FILE *)fp))
-					printf(buffer, "%s\n");
+						printf(buffer, "%s\n");
+					fclose(fp);
 				}
-				fclose(fp);
+				else{
+					sprintf(buffer, "파일이 없습니다.");
+				}
+				
 			}					
-			
-			else if(!strncasecmp(rcvBuffer, "exec ", 5)){
-				char *Etoken;
-                                char *Estr[5];
-                                int i = 0;
-				int ret;
+			//6-2
+			else if(!strncasecmp(rcvBuffer, "exec ", 5)) {
+                                i = 0;
+				char *command;
 
-				Etoken = strtok(rcvBuffer, " ");
-				while (Etoken != NULL) {
-					Estr[i++] = Etoken;
-					Etoken = strtok(NULL, " ");
-				}
+				command = strtok(NULL, "\0");
 				
-				ret = system(Estr[1]);
-				
-				if(!ret)
-					printf("command Success!!\n");
+				printf("command:%s\n", command);
+				int result = system(command);
+
+				if(result)
+					sprintf(buffer, "[%s] 명령어가 실패하였습니다.", command);
 				else
-					printf("command Failed!!\n");
+					sprintf(buffer, "[%s] 명령어가 성공하였습니다.", command);
 			}
 			else
 				strcpy(buffer, "무슨 말인지 모르겠습니다ㅠ_ㅠ");
