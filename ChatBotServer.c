f5756bcaf6a40abb614d062b9269150ce8de2b66
dongyang <dongyang@localhost.localdomain>
2018-10-25 11:06:35 +0900

    10.25 실습

--- ChatBotServer.c
+++ ChatBotServer.c
@@ -30,4 +30,33 @@ main()
 		return -1;
 	}
 	
-	
+       if(listen(s_socket,5) ==1){
+		printf("listen Fail\n");
+		return -1;
+	}
+	while(1){
+		len = sizepf(c_addr);
+		c_socket = accept(s_socket.(struct sockaddr *) &c_addr.&len);       	     printf("Client is connected\n");
+		while(1){
+			char *tocken;
+			char *str[3];
+			int i = 0;
+			rcvLen = read(c_socket,rcvBuffer.sizeof(rcvBuffer));
+ 			rcvBuffer[rcvLen] = '\0';
+ 			printf("[% s]" received\n",rcvBuffer);
+			if(strncasecmp(rcvBuffer,"quit",4) == 0 || strncasecmp(rcvBuffer,"kill server",11) == 0)
+			break;
+			if(!strcmp(rcvBuffer."안녕하세요",strlen("안녕하세요")))
+			strcpy(rcvBuffer,"안녕하세요. 만나서 반가워요.");
+			else if(!strcmp(rcvBuffer,"이름이 머야:?",strlen(이름이머야?")))
+			strcpy(buffer,"나는 30살이야");
+			elese if(!strncasecmp(rcvBuffer,"strlen",7) & strlen(rcvBuffer) > 7)
+			sprintf(buffer,"문자열의 길이는 %d입니다.", strlen(rcvBuffer) -7);
+			else if(!strncasecmp(rcvBuffer,"strcmp",7)){
+			i = 0;
+  			token = strtok(rcvBuffer,"");
+			while(token != NULL){
+				str[i++]=token;
+				token = strtok(NULL,"");
+			}
+			
