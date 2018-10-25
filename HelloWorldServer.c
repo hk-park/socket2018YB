3e966442d251e82fb16570e08a337355f0a1f270
xscdvf21 <43835301+xscdvf21@users.noreply.github.com>
2018-10-25 10:08:43 +0900

    Update HelloWorldServer.c

--- HelloWorldServer.c
+++ HelloWorldServer.c
@@ -5,139 +5,116 @@
 // 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
 #define PORT 9000
 //#define PORT 10000
-
+#define BUFSIZE 10000 
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
-char buffer[100] = "Hi, I'm server\n";
-
+char buffer[BUFSIZE] = "Hi, I'm server\n";
+ 
 main( )
 {
-        int   c_socket, s_socket;
-        struct sockaddr_in s_addr, c_addr;
-        int   len;
-        int   n,a,x;
-	FILE *fp;
-        int rcvLen;
-	char buffer2[100];
-        char rcvBuffer[100],*jdy1,*jdy2;
-        char jdyBuffer[100] = " ";
-
-        s_socket = socket(PF_INET, SOCK_STREAM, 0);
-
-        memset(&s_addr, 0, sizeof(s_addr));
-        //s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-        s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        s_addr.sin_family = AF_INET;
-        s_addr.sin_port = htons(PORT);
-
-        if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
-                printf("Can not Bind\n");
-                return -1;
-        }
-
-        if(listen(s_socket, 5) == -1) {
-                printf("listen Fail\n");
-                return -1;
-        }
-
-        while(1) {
-                len = sizeof(c_addr);
-                c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-                //3-3.클라이언트가 접속했을 때 "Client is connected" 출력
-                printf("Client is connected\n");
-                while(1){
-                        rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
-                        rcvBuffer[rcvLen] = '\0';
-			buffer[0] ='\0';
-                        printf("[%s] received\n", rcvBuffer);
-                        if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
-                        {
-                                break;
-                        }
-                        else if(!strncmp(rcvBuffer, "안녕하세요",strlen("안녕하세요")))
-                          {
-                                strcpy(buffer,"안녕하세요 만나서 반가워요 .");
-                                n=strlen(buffer);
-                                write(c_socket,buffer,n);
-                          }
-                        else if(!strncmp(rcvBuffer,"이름이 머야?",strlen("이름이 머야?")))
-                         {
-                                strcpy(buffer,"내이름을 정달영이야");
-                                n = strlen(buffer);
-                                write(c_socket,buffer,n);
-                         }
-                        else if(!strncmp(rcvBuffer,"몇 살이야?",strlen("몇 살이야?")))
-                         {
-                                strcpy(buffer,"나는 100살이야.");
-                                n =strlen(buffer);
-                                write(c_socket,buffer,n);
-                        }
-
-                        else if(!strncasecmp(rcvBuffer,"strlen ",7))
-                        {
-                         a  = strlen(rcvBuffer)-7;
-                         sprintf(buffer,"길이 : %d",a);
-                         write(c_socket, buffer,strlen(buffer));
-                        }
-
-                        else if (!strncasecmp(rcvBuffer,"strcmp ",7))
-                        {
-                          strtok(rcvBuffer," ");
-                          jdy1=strtok(NULL," ");
-                          jdy2=strtok(NULL," ");
-                          x=strcmp(jdy1,jdy2);
-
-                          sprintf(buffer,"%d\n",x);
-                          write(c_socket,buffer,strlen(buffer));
-
-                        }
-
-			else if (!strncasecmp(rcvBuffer,"readfile",8))
-			{
-				strtok(rcvBuffer," ");
-				jdy1 = strtok(NULL," ");
-				jdy2 = jdy1;
-				fp=fopen(jdy2,"r");	
-				
-				if(fp)
-				{
-					while(fgets(buffer2,100,(FILE *)fp))
-						strcat(buffer, buffer2);
+	int   c_socket, s_socket;
+	struct sockaddr_in s_addr, c_addr;
+	int   len;
+	int   n;
+	int rcvLen;
+	char rcvBuffer[BUFSIZE];
+ 	s_socket = socket(PF_INET, SOCK_STREAM, 0);
+	
+	memset(&s_addr, 0, sizeof(s_addr));
+	//s_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	s_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	s_addr.sin_family = AF_INET;
+	s_addr.sin_port = htons(PORT);
+ 
+	if(bind(s_socket, (struct sockaddr *) &s_addr, sizeof(s_addr)) == -1) {
+		printf("Can not Bind\n");
+		return -1;
+	}
+ 
+	if(listen(s_socket, 5) == -1) {
+		printf("listen Fail\n");
+		return -1;
+	}
+ 	
+	while(1) {
+		len = sizeof(c_addr);
+		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
+		printf("Client is connected\n");
+		while(1){
+			char *token;
+			char *str[3];
+			int i = 0;
+			rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
+			rcvBuffer[rcvLen] = '\0';
+			printf("[%s] received\n", rcvBuffer);
+			if(strncasecmp(rcvBuffer, "quit", 4) == 0 || strncasecmp(rcvBuffer, "kill server", 11) == 0)
+				break;
+			else if(!strncmp(rcvBuffer, "안녕하세요", strlen("안녕하세요")))
+				strcpy(buffer, "안녕하세요. 만나서 반가워요.");
+			else if(!strncmp(rcvBuffer, "이름이 머야?", strlen("이름이 머야?")))
+				strcpy(buffer, "내 이름은 박홍규야.");
+			else if(!strncmp(rcvBuffer, "몇 살이야?", strlen("몇 살이야?")))
+				strcpy(buffer, "나는 30살이야.");
+			else if(!strncasecmp(rcvBuffer, "strlen ", 7))
+				sprintf(buffer, "내 문자열의 길이는 %d입니다.", strlen(rcvBuffer)-7);
+			else if(!strncasecmp(rcvBuffer, "strcmp ", 7)){
+				i = 0;
+				token = strtok(rcvBuffer, " ");
+				while(token != NULL){
+					str[i++] = token;
+					token = strtok(NULL, " ");
 				}
-				fclose(fp);
-				write(c_socket,buffer,strlen(buffer));
-
-			}
-
-			else if (!strncasecmp(rcvBuffer,"exec",4))
-					{
-
-							strtok(rcvBuffer," ");
-							jdy1 = strtok(NULL,"NULL");
-							jdy2 = jdy1;
-							int ret = system(jdy2);
-							
-							if(!ret)
-							  sprintf(buffer,"%s,command is Success",jdy2);
-							else
-							  sprintf(buffer,"%s,command is failed",jdy2);
-							 n =strlen(buffer);
-                         				write(c_socket, buffer, n);
-						
+				if(i<3)
+					sprintf(buffer, "문자열 비교를 위해서는 두 문자열이 필요합니다.");
+				else if(!strcmp(str[1], str[2])) //같은 문자열이면,
+					sprintf(buffer, "%s와 %s는 같은 문자열입니다.", str[1], str[2]);
+				else
+					sprintf(buffer, "%s와 %s는 다른 문자열입니다.", str[1], str[2]);
+					
+			}else if (!strncasecmp(rcvBuffer, "readfile ", 9)) {
+				i=0;
+				token = strtok(rcvBuffer, " ");
+				while(token != NULL){
+					str[i++] = token;
+					token = strtok(NULL, " ");
+				}
+				//str[0] = readfile
+				//str[1] = filename
+				if(i<2)
+					sprintf(buffer, "readfile 기능을 사용하기 위해서는 readfile <파일명> 형태로 입력하시오.");
+				FILE *fp = fopen(str[1], "r");
+				if(fp){
+					char tempStr[BUFSIZE];
+					memset(buffer, 0, BUFSIZE);
+					while(fgets(tempStr, BUFSIZE, (FILE *)fp)){
+						strcat(buffer, tempStr);
 					}
-	                    else{
-                        n =strlen(buffer);
-                        write(c_socket, jdyBuffer, n);
-                        }
-
-                }
-                close(c_socket);
-                if(!strncasecmp(rcvBuffer, "kill server", 11))
-                        break;
-        }
-        close(s_socket);
+					fclose(fp);
+				}else{
+					sprintf(buffer, "파일이 없습니다.");
+				}
+			}else if (!strncasecmp(rcvBuffer, "exec ", 5)) {
+				char *command;
+				token = strtok(rcvBuffer, " "); //exec
+				command = strtok(NULL, "\0");
+				printf("command: %s\n", command);
+				int result = system(command);
+				if(result)
+					sprintf(buffer, "[%s] 명령어가 실패하였습니다.", command);
+				else
+					sprintf(buffer, "[%s] 명령어가 성공하였습니다.", command);
+			}else
+				 strcpy(buffer, "무슨 말인지 모르겠습니다.");
+			n = strlen(buffer);
+			write(c_socket, buffer, n);
+		}
+		close(c_socket);
+		if(!strncasecmp(rcvBuffer, "kill server", 11))
+			break;
+	}	
+	close(s_socket);
 }
-
 
 
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
