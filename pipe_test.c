cea985ec861b534339a242fba8f49bcfd665729f
211 <211@localhost.localdomain>
2018-11-22 11:54:37 +0900

    pipe를 활용한 프로세스통신(IPC) 예제 추가

--- pipe_test.c
+++ pipe_test.c
@@ -4,11 +4,11 @@
 #include <string.h>
 
 main() {
-	int fd[2];				// 파이프로 사용할 디스크럽터
+	int fd[2], fd2[2];				// 파이프로 사용할 디스크럽터
 	char buf[255];
 	int pid;
 
-	if(pipe(fd) < 0) {			// pipe 생성, 생성 실패 시 프로그램 종료
+	if(pipe(fd) < 0  || pipe(fd2) < 0) {			// pipe 생성, 생성 실패 시 프로그램 종료
 		printf("[ERROR] pipe erroe\n");
 		exit(0);
 	}
@@ -17,11 +17,16 @@ main() {
 		memset(buf, 0x00, 255);
 		read(fd[0], buf, sizeof(buf));
 		printf("[PARENT] Child message : %s\n", buf);
+		memset(buf, 0x00, 255);
+		sprintf(buf, "[%d] Hello, I'm Parent.", getpid());
+		write(fd2[1], buf, strlen(buf));
 	}
 	else if(pid == 0) {			// 자식 프로세스, 파이프를통해 부모에게 값 전달
 		memset(buf, 0x00, 255);
 		sprintf(buf, "[%d] Hello, I'm Child.", getpid());
 		write(fd[1], buf, strlen(buf));
+		read(fd2[0], buf, sizeof(buf));
+		printf("[CHILD] Parent message : %s\n", buf);
 	}
 	else {
 		printf("[ERROR] fork() failed\n");
