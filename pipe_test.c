a10d3766cbc843c04dbb081e20529aa457ae7b07
LeeSunR <root@localhost.localdomain>
2018-11-22 11:40:40 +0900

    파이프 실습 완료

--- pipe_test.c
+++ pipe_test.c
@@ -7,6 +7,7 @@
 
 int main(){
 	int fd[2];
+	int fd2[2];
 	char buf[255];
 	int pid;
 
@@ -15,14 +16,29 @@ int main(){
 		exit(1);
 	}
 
+	if(pipe(fd2)<0){
+		printf("[ERROR] pipe error\n");
+		exit(1);
+	}
+
 	pid = fork();
 
 	if(pid>0){ //부모 프로세스
 		memset(buf,0,255);
+		sprintf(buf,"[%d] Hello, I'm parent",getpid());
+		write(fd2[1],buf,strlen(buf));		
+
+		memset(buf,0,255);
 		read(fd[0],buf,sizeof(buf));
 		printf("[PARENT] child message :%s\n",buf);
+
 	}
+
 	else if(pid == 0){ //자식 프로세스
+		memset(buf,0,255);
+		read(fd2[0],buf,255);
+		printf("[CHILD] server message :%s\n",buf);
+
 		memset(buf,0,255);
 		sprintf(buf,"[%d] Hello, I'm child",getpid());
 		write(fd[1],buf,strlen(buf));//파이프로 값 전달
