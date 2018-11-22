aa78148fb22e38a62d9ac3927a229d662acf2f47
211 <211@localhost.localdomain>
2018-11-22 11:53:45 +0900

    [11.22] 파이프 실습

--- pipe.c
+++ pipe.c
@@ -1,30 +1,36 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<string.h>
-
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 int main(){
-	int fd[2]; //pipe로 사용할 파일디스크립트
+	int fd[2], fd2[2]; //pipe로 사용할 파일디스크립터
 	char buf[255];
 	int pid;
-
-	//pipe 생성 실패 시 프로그램 종
-	if(pipe(fd) <0){료
-		printf("[Error] pipe errer!!1\n")l
-		exit(1);
+	if(pipe(fd) < 0 || pipe(fd2) < 0){ //pipe 생성. 생성 실패 시, 프로그램 종료
+		printf("[ERROR] pipe error\n");
+		exit(0);
 	}
-	pid=fork();
-	if(pid>0){
-		//부모 프로세스에서는 자식프로세스가 전달하는 파이프를 통해 전달하는 값을 출력	
-		memset(buf, 0x00,255);
-		read(fd[0],buf,sizeof(buf));
-		printf("[PARENT] 클라이언트 메세지:%s\n",buf);
-	}else if(pid==0){
-		//자식 프로세스에서는 파이프를 통해 부모 프로세스에게 값을 전달
-		memset(buf, 0x00,255);
-		sprintf(buf,"[%d] 안녕, 나는 클라이언트야",getpid());
-		write(fd[1],buf,strlen(buf));
+	pid = fork();
+	if(pid > 0){
+		//부모 프로세스
+		//자식프로세스가 파이프를 통해 전달하는 값을 출력
+		memset(buf, 0x00, 255);
+		read(fd[0], buf, sizeof(buf));
+		printf("[PARENT] child message: %s\n", buf);
+		memset(buf, 0x00, 255);
+		sprintf(buf, "[%d] Hello, I'm parent.", getpid());
+		write(fd2[1], buf, strlen(buf));
+	}else if (pid == 0){
+		//자식 프로세스
+		//파이프를 통해 부모 프로세스에게 값을 전달
+		memset(buf, 0x00, 255);
+		sprintf(buf, "[%d] Hello, I'm child.", getpid());
+		write(fd[1], buf, strlen(buf));
+		read(fd2[0], buf, sizeof(buf));
+		printf("[CHILD] parent message: %s\n", buf);
+		
 	}else{
-		printf("[Error] fork() failed!!!\n");
-		exit(1);
+		printf("[ERROR] fork() failed\n");
+		exit(0);
 	}
 }
