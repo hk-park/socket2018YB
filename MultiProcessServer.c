7afe321c06413390793ec561a5f412687521fd39
211 <211@localhost.localdomain>
2018-11-22 11:12:34 +0900

    [11.22] signal 처리 추가

--- MultiProcessServer.c
+++ MultiProcessServer.c
@@ -3,6 +3,8 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <string.h>
+#include <signal.h>
+#include <sys/wait.h>
 // 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
 #define PORT 9000
 //#define PORT 10000
@@ -10,6 +12,8 @@
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
 char buffer[100] = "Hi, I'm server\n";
+int numClient = 0;
+void sig_handler();
 void do_service(int c_socket);
 main( )
 {
@@ -17,6 +21,8 @@ main( )
 	struct sockaddr_in s_addr, c_addr;
 	int len;
 	int pid;
+	signal(SIGCHLD, sig_handler);
+
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
@@ -38,7 +44,8 @@ main( )
 	while(1) {
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
-		
+		numClient++;
+		printf("현재 %d개의 클라이언트가 접속하였습니다.\n", numClient);
 		if((pid = fork()) > 0){
 			close(c_socket);
 			continue;
@@ -129,4 +136,12 @@ void do_service(int c_socket){
 			}
 		}
 		close(c_socket);
+}
+
+void sig_handler(int signo){
+	int pid, status;
+	pid = wait(&status);
+	printf("1개의 클라이언트가 종료되어 %d개의 클라이언트가 접속되어있습니다.\n",--numClient);
+	printf("[pid] = [%d] is terminated. Status = %d\n\n", pid, status);
+		
 }
