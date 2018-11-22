f7e6acdf96a1b202aab99df08faee23e427a1523
211 <211@localhost.localdomain>
2018-11-22 11:40:02 +0900

    11.22 signal추가

---
+++ pipe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+int main(){
+	int fd[2];//pipe로 사용할 파일디스크립터
+	char buf[255];
+	int pid;
+
+	if(pipe(fd) < 0 ){//pipe생성 생성실패시 프로그램종료
+		printf("[ERROR] pipe error\n");
+		exit(0);
+	}
+	pid = fork();
+	if ( pid > 0 ){
+		//부모 프로세스
+		//자식프로세스가 파이프를 통해 전달하는 값을  출력
+		memset(buf, 0x00, 255);
+		read(fd[0], buf, sizeof(buf));
+		printf("[PARENT] child message: %s", buf);
+	}else if (pid ==0){
+		//자식 프로세스
+		//파이프를 통해 부모 프로세스에게 값을  전달
+		memset(buf, 0x00, 255);
+		sprintf(buf,"[%d]Hello,I'm child", getpid());
+		write(fd[1], buf, strlen(buf));
+	}else{
+		printf("[ERROR]fork() failed\n");
+		exit(0);
+	}
+}
