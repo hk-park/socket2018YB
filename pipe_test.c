bb803dbc3e49cc3bda8acdbe67f4d6b85edc0abc
dongyang <211@localhost.localdomain>
2018-11-22 11:51:54 +0900

    [11.22]파이프 양방향 실습

--- pipe_test.c
+++ pipe_test.c
@@ -4,10 +4,11 @@
 #include<stdlib.h>
 int main(){
 	int fd[2];//pipe로 사용할때 파일디스크립터
+	int fd2[2];
 	char buf[255];
 	int pid;
 
-	if(pipe(fd) <0  ){//pipe생성
+	if(pipe(fd) <0 || pipe(fd2)  ){//pipe생성
 	printf("[ERROR] pipe error\n");
 	exit(0);
 		
@@ -17,13 +18,17 @@ int main(){
 	memset(buf , 0x00, 255);
 	read(fd[0], buf,sizeof(buf));
 	printf("[PARENT] child message %s\n ",buf);
-	
+	memset(buf , 0x00 ,255);
+	sprintf(buf, "[%d] hello, im parent",getpid());
+	write(fd2[1] , buf, strlen(buf));	
 	//부모 프로세스
 	}//자식프로세스가 파이프를 통해 전달하는 값을 출력
 	else if(pid==0){
 	memset(buf,0x00,255);
-	sprintf(buf ,"[%d]Hello Im client ",getpid());
+	sprintf(buf ,"[%d]Hello Im child ",getpid());
 	write(fd[1], buf, strlen(buf));
+	read(fd2[0], buf ,sizeof(buf));
+	printf("[child] parent message %s\n", buf);
 //	write(c_socket , buf ,strlen(buf))
 	//자식 프로세스
 	//파이프를통해 부모프로세스에게 값을 전달
