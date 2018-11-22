2b3b4d5c7242153d2531750ba23933da3fc19134
211 <211@localhost.localdomain>
2018-11-22 11:52:58 +0900

    파이프

--- pipe.c
+++ pipe.c
@@ -3,11 +3,11 @@
 #include <string.h>
 #include <stdlib.h>
 int main(){
-	int fd[2];//pipe로 사용할 파일디스크립터
+	int fd[2],fd2[2];//pipe로 사용할 파일디스크립터
 	char buf[255];
 	int pid;
 
-	if(pipe(fd) < 0 ){//pipe생성 생성실패시 프로그램종료
+	if(pipe(fd) < 0 || pipe(fd2) < 0 ){//pipe생성 생성실패시 프로그램종료
 		printf("[ERROR] pipe error\n");
 		exit(0);
 	}
@@ -18,11 +18,15 @@ int main(){
 		memset(buf, 0x00, 255);
 		read(fd[0], buf, sizeof(buf));
 		printf("[PARENT] child message: %s", buf);
+		memset(buf, 0x00, 255);
+		sprintf(buf,"[%d]Hello.I'm parent",getpid());
+		write(fd2[1], buf, strlen(buf));
 	}else if (pid ==0){
 		//자식 프로세스
 		//파이프를 통해 부모 프로세스에게 값을  전달
 		memset(buf, 0x00, 255);
 		sprintf(buf,"[%d]Hello,I'm child", getpid());
+		read(fd2[1], buf, sizeof(buf));
 		write(fd[1], buf, strlen(buf));
 	}else{
 		printf("[ERROR]fork() failed\n");
