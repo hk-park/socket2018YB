4fbe748e0f41eee49e02f121705482f5acfdf466
root <root@localhost.localdomain>
2018-11-28 03:58:31 +0900

    kill server 에러 안나게 수정 완벽한 kill server은 아님.

--- MultiProcessServer.c
+++ MultiProcessServer.c
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <unistd.h>
+//#include <sys/time.h>
 
 #define PORT 9000
 #define BUFSIZE 10000
@@ -19,9 +21,13 @@ char clearBuffer[BUFSIZE];
 
 int len, n;
 int concli = 0;
+int fd[2];
+int fd1[2];
+int sigcheck = 0;//exec 명령어등 할때 시그널 실행 방지용
+int c_socket, s_socket;
+//struct timeval tv;
 
 int main(){
-	int c_socket, s_socket;
 	int pid;
 	//s_socket: 클라이언트의 접속을 기다리는 소켓
 	//c_socket: 클라이언트에게 메시지를 전달하기 위한 소켓
@@ -34,6 +40,9 @@ int main(){
 	s_addr.sin_addr.s_addr = htonl(INADDR_ANY);//IP주소 저장
 	s_addr.sin_family = AF_INET;
 	s_addr.sin_port = htons(PORT);
+
+	//tv.tv_sec = 7;
+	//tv.tv_usec = 0;
 	
 	if(bind(s_socket, (struct sockaddr *)&s_addr, sizeof(s_addr)) == -1){
 		printf("[ERR] Cannot bind\n");
@@ -44,19 +53,37 @@ int main(){
 		printf("[ERR] Cannot listen\n");
 		return -1;
 	}
+
+	if(pipe(fd) < 0 || pipe(fd1) < 0){
+		printf("[ERROR] pipe error\n");
+		exit(0);
+	}
 	
 	while(1){
+		/*printf("kill server 대기중...\n");
+		
+		if(!strncasecmp(rcvBuffer, "kill server", 11)){
+			break;
+		}*/
+		printf("클라이언트 기달리는 중...\n");
 		len = sizeof(c_aadr);
 		c_socket = accept(s_socket, (struct sockaddr *)&c_aadr, &len);
+		//setsockopt(c_socket, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv, sizeof(struct timeval));
 		concli++;
 		printf("Client is connected\n");
 		printf("현재 %d개의 클라이언트가 접속 하였습니다.\n", concli);
 		if((pid=fork())>0){
 			close(c_socket);
 			continue;
-		}else if(pid == 0){
+		}else if(pid == 0){//이때 자식 서버가 만들어 진다.
 			close(s_socket);
+			sigcheck = 1;
 			do_service(c_socket);
+			/*if(!strncasecmp(rcvBuffer, "kill server", 11)){
+				exit(0);
+			}else if(!strncasecmp(rcvBuffer, "quit", 4)){
+				exit(0);
+			}*/
 			exit(0);
 		}else{
 			//에러 처리
@@ -162,27 +189,43 @@ void do_service(int c_socket){
 			}
 		}
 		else {
+			//strcpy(rcvBuffer,clearBuffer);
 			strcpy(buffer, "무슨말이오.??\n");
 		}
-		/*if(strncasecmp(rcvBuffer, "kill server", 11) == 0){
+
+		if(strncasecmp(rcvBuffer, "kill server", 11) == 0){
 			break;
-		}*/
-		n = strlen(buffer);
-		write(c_socket, buffer, n);
+		}
+		/*클라이언트 쪽에서 quit를 하게 되면 클라이언트가 종료되었지만 
+		자식 서버쪽에서는 'write'을 실행한다. 이러면 분명히 오류가 발생이 된다.*/
+		if(strncasecmp(rcvBuffer, "quit", 4) != 0){
+			n = strlen(buffer);
+			write(c_socket, buffer, n);
+		}else{
+			break;
+		}
 	}
 
-	write(c_socket, buffer, strlen(buffer));
 	close(c_socket);
-	/*if(!strncasecmp(rcvBuffer, "kill server", 11)){
-		break;
-	}*/
+
+	if(!strncasecmp(rcvBuffer, "kill server", 11)){
+		return;
+	}else if(!strncasecmp(rcvBuffer, "quit", 4)){
+		return;
+	}
 }
 
 void sig_handler(){
-	int pid;
-	int status;
-	pid = wait(&status);
-	printf("pid[%d] is terminated. status = %d\n", pid, status);
-	concli--;
-	printf("1개의 클라이언트가 종료되어 현재 %d가 접속 되어 있습니다.\n", concli);
+	//read(fd[0],rcvBuffer, sizeof(rcvBuffer));
+	if(sigcheck == 0){
+		int pid;
+		int status;
+		pid = wait(&status);
+		printf("pid[%d] is terminated. status = %d\n", pid, status);
+		concli--;
+		printf("1개의 클라이언트가 종료되어 현재 %d개가 접속 되어 있습니다.\n", concli);
+		return;
+	}else if(sigcheck == 1){
+		return;
+	}
 }
