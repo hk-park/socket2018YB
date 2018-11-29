8ec8e90be185d2a4123b21e45de3f89ea097e370
LeeSunR <root@localhost.localdomain>
2018-11-29 11:41:23 +0900

    [11.29]MultiThreadServer

---
+++ pthread_mutex.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int ncount = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *do_sum(void *data);
+
+
+int main(){
+	int trd_id;
+	pthread_t p_thread[3];
+	int status;
+	int a = 5, b = 10;
+
+	trd_id = pthread_create(&p_thread[0], NULL, do_sum, (void *)&a);
+	trd_id = pthread_create(&p_thread[1], NULL, do_sum, (void *)&b);
+	pthread_join(p_thread[0], (void **)&status);
+	pthread_join(p_thread[1], (void **)&status);
+	printf("main program is ended\n");
+	pthread_mutex_destroy(&mutex);
+	return 0;
+}
+
+void *do_sum(void *data){
+	int i;
+	int n = *(int*)data;
+	for(i=0;i<n;i++){
+		printf("[%x]sum: %d\n", pthread_self(),ncount);
+		pthread_mutex_lock(&mutex);
+		ncount++;
+		pthread_mutex_unlock(&mutex);
+	}
+	
+}
