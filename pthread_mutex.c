d0cf20ef31a6e21d1d4121e2197583cbe2f878a2
211 <211@localhost.localdomain>
2018-11-29 11:25:21 +0900

    [11/29] pthread_mutex 예제 추가

---
+++ pthread_mutex.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int ncount = 0;
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void *do_sum(void *data) {
+	int i;
+	int n = *((int *)data);
+
+	for(i = 0; i < n; i++) {
+		printf("[%x] sum : %d\n", pthread_self(), ncount);
+		pthread_mutex_lock(&mutex);			// 자신의 스레드 아이디값 받는 함수
+		ncount++;
+		pthread_mutex_unlock(&mutex);							
+	}
+}
+
+int main() {
+	int thr_id;
+	pthread_t p_thread[2];
+	int status;
+	int a = 5;
+	int b = 10;
+	thr_id = pthread_create(&p_thread[0], NULL, do_sum, (void **)&a);
+	thr_id = pthread_create(&p_thread[1], NULL, do_sum, (void **)&b);
+
+	pthread_join(p_thread[0], (void **)&status);
+	pthread_join(p_thread[1], (void **)&status);
+
+	printf("main prigram is ended\n");
+
+	pthread_mutex_destroy(&mutex);
+	return 0;
+}
