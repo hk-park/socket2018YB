1a62fcec521f3a3be2d489f444e0b4d87ac3d17c
dongyang <dongyang@localhost.localdomain>
2018-12-06 10:45:34 +0900

    [12.06] 멀티스레드 실습 완료

--- MultiThreadServer.c
+++ MultiThreadServer.c
@@ -4,7 +4,6 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <string.h>
-#include <unistd.h>
 #include <pthread.h>
 // 2-1. 서버 프로그램이 사용하는 포트를 9000 --> 10000으로 수정 
 #define PORT 9000
@@ -13,14 +12,16 @@
 // 2-2. 클라이언트가 접속했을 때 보내는 메세지를 변경하려면 buffer을 수정
 //char buffer[100] = "hello, world\n";
 char buffer[BUFSIZE] = "Hi, I'm server\n";
-void *do_service(void *data);
+void *do_service(void *argv);
+pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+int numClient=0;
 main( )
 {
 	int   c_socket, s_socket;
 	struct sockaddr_in s_addr, c_addr;
 	int len;
 	pthread_t pthread;
-	int thr_id;
+	int status;
  	s_socket = socket(PF_INET, SOCK_STREAM, 0);
 	
 	memset(&s_addr, 0, sizeof(s_addr));
@@ -41,18 +42,22 @@ main( )
 	while(1) {
 		len = sizeof(c_addr);
 		c_socket = accept(s_socket, (struct sockaddr *) &c_addr, &len);
+		pthread_mutex_lock(&mutex);
+		numClient++;
+		pthread_mutex_unlock(&mutex);
 		//3-3.클라이언트가 접속했을 때 "Client is connected" 출력
-		thr_id=pthread_create(&pthread,NULL,do_service,(void *)&c_socket);
-		printf("Client is connected\n");
+		printf("%dth Client is connected\n",numClient);
+		status = pthread_create(&pthread,NULL,do_service, (void *)&c_socket);
 	}
+	pthread_mutex_destroy(&mutex);
 	close(s_socket);
 }
-void *do_service(void *data){
+void *do_service(void *argv){
 	int   len;
         int   n;
         int rcvLen;
 	char rcvBuffer[BUFSIZE];
-	int c_socket=*((int *)data);
+	int c_socket=*((int *)argv);
 	while(1){
                         rcvLen = read(c_socket, rcvBuffer, sizeof(rcvBuffer));
                         rcvBuffer[rcvLen] = '\0';
@@ -126,6 +131,10 @@ void *do_service(void *data){
                         n=strlen(buffer);
                         write(c_socket, buffer, n);
         	}
+		pthread_mutex_lock(&mutex);
+		numClient--;
+		pthread_mutex_lock(&mutex);
+		printf("현재 %d개의 클라이언트가 접속 중입니다.\n",numClient);
                 close(c_socket);
  }
     
